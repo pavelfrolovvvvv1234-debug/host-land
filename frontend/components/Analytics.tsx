66ab5aab075146a354ea84a24d3a8e79e2f17725
pavel <pavelfrolovvvvv1234@gmail.com>
2025-12-01 23:04:04 +0300

    Add Yandex Metrika counter (ID: 105570768) with full tracking features

--- frontend/components/Analytics.tsx
+++ frontend/components/Analytics.tsx
@@ -44,37 +44,34 @@ export function Analytics() {
       )}
 
       {/* Yandex Metrika */}
-      {yandexId && (
-        <>
-          <Script id="yandex-metrika" strategy="afterInteractive">
-            {`
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              
-              ym(${yandexId}, "init", {
-                ssr:true,
-                webvisor:true,
-                clickmap:true,
-                ecommerce:"dataLayer",
-                accurateTrackBounce:true,
-                trackLinks:true
-              });
-            `}
-          </Script>
-          <noscript>
-            <div>
-              <img
-                src={`https://mc.yandex.ru/watch/${yandexId}`}
-                style={{ position: "absolute", left: "-9999px" }}
-                alt=""
-              />
-            </div>
-          </noscript>
-        </>
-      )}
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105570768', 'ym');
+          
+          ym(105570768, 'init', {
+            ssr:true,
+            webvisor:true,
+            clickmap:true,
+            ecommerce:"dataLayer",
+            accurateTrackBounce:true,
+            trackLinks:true
+          });
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/105570768"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
 
       {/* Bing Webmaster Tools - Meta tag will be added via metadata in layout */}
       {/* Script verification is handled via meta tag */}
